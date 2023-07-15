ccccea1f6843f5f764e419ec95603dd0b27cedc0
Naeem-j3 <naeemjamool24@gmail.com>
2023-07-15 01:40:09 -0600

    add company and customer

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\CustomerResource;
+use App\Models\Customer;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $customers = Customer::paginate(10);
+        return CustomerResource::collection($customers);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required',Rule::unique('customers', 'name')],
+            'phone' => [ 'digits:10', Rule::unique('customers', 'phone'),
+            ],
+        ]);
+        $customer= Customer::create($request->all());
+
+        return response()->json(['message' => 'Customer has been successsfully create',new CustomerResource($customer)]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Customer $customer)
+    {
+        return new CustomerResource($customer);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Customer $customer)
+    {
+        $request->validate([
+            'name' => [Rule::unique('customers', 'name')],
+            'phone' => [ 'digits:10', Rule::unique('customers', 'phone'),
+            ],
+        ]);
+        $customer->update($request->all());
+        return response()->json(['message' => 'Customer has been successsfully update',new CustomerResource($customer)]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Customer $customer)
+    {
+        $customer->delete();
+        return response()->json(['message' => 'Customer has been successsfully delete']);
+    }
+}
